bf0cd6d044b995d85dde10273a5d434fbac2a995
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-06-10 22:16:18 +0500

    products done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { createRandomProduct } from "src/homeworks/ts1/3_write";
 
 function App() {
+  // const get = () => {
+  //   return createRandomProduct('2023-06-06T12:06:56.957Z');
+  // };
+  // console.log(get());
   return (
     <div className="App">
       <header className="App-header">
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -1,6 +1,29 @@
+import { getData } from 'src/mock-data/mock-products';
+
 /**
  * Функции написанные здесь пригодятся на последующих уроках
  * */
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+type Product = Partial<Category> & {
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+type Cost = Category &
+  Pick<Product, 'createdAt'> & {
+    amount: number;
+    category: Category;
+    readonly type: 'Cost';
+  };
+type Profit = Exclude<Cost, 'type'> & {
+  readonly type: 'Profit';
+};
 
 /**
  * Нужно создать тип Category, он будет использоваться ниже.
@@ -44,7 +67,48 @@
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+/**
+  https://fakestoreapi.com/products/?limit=$
+ * */
+type fakeProduct = {
+  id: string;
+  title: string;
+  price: string;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: string;
+    count: string;
+  };
+};
+
+const getRandomProducts = (count = 1) => {
+  const products = getData() as unknown as fakeProduct[];
+  return products;
+};
+
+export const createRandomProduct = (createdAt: string) => {
+  const mockProducts: fakeProduct[] = getRandomProducts();
+  const products: Product[] = mockProducts.map((item) => {
+    const { id, description, image, title, price, category } = item;
+    const oldPrice = parseInt((parseInt(price) * Math.random() * 10).toFixed(2));
+    const product: Product = {
+      desc: description,
+      name: title,
+      photo: image,
+      oldPrice: oldPrice,
+      category: { id: id, name: category, photo: image },
+      createdAt: createdAt,
+      price: parseInt(price),
+      id: id,
+    };
+    return product;
+  });
+  const rndIndx = Math.floor(Math.random() * mockProducts.length);
+  return products[rndIndx];
+};
 
 /**
  * Создает случайную операцию (Operation).
