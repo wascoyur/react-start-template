f104870b4bbea7c4cfa4ae9d0bcfde9b9137d1c7
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-06-18 17:14:38 +0500

    change start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { createRandomProduct } from "src/homeworks/ts1/3_write";
 
 function App() {
-  // const get = () => {
-  //   return createRandomProduct('2023-06-06T12:06:56.957Z');
-  // };
-  // console.log(get());
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>powered by yuriy.vasilev</p>
       </header>
     </div>
   );
