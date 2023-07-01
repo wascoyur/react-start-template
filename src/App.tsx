7b1d2a52d16f6063f1c0b831aeedc77d2a9f0a98
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-07-01 19:02:25 +0500

    реализовано модальное окно

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import './components/modal/Modal-window.scss';
 import { ModalWindow } from './components/modal/ModalWindow';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div className="App">
+      <button onClick={() => setIsVisible(true)}> Show modal</button>
       <ModalWindow
         onClick={() => {
+          setIsVisible((v) => !v);
           console.log(`click`);
         }}
-        visible={true}
+        visible={isVisible}
       >
-        some text inside modal window
+        <div>some text inside modal window</div>
       </ModalWindow>
     </div>
   );
--- src/components/modal/ModalWindow.tsx
+++ src/components/modal/ModalWindow.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import './Modal-window.scss';
+import ReactDOM from 'react-dom';
 
 type Props = {
   onClick: () => void;
   visible: boolean;
   children?: React.ReactNode;
+  pp?: React.ReactNode;
 };
 
 const ModalWindow = (props: Props) => {
-  const { children } = props;
-  return (
-    <div className="modal-layout">
-      <div className="modal-window">{children}</div>
-    </div>
-  );
+  const { children, onClick, visible } = props;
+  const modal = () => {
+    return (
+      <div className="modal-layout">
+        <div className="modal-window">
+          <div className="close-wrapper">
+            <div className="close-icon" onClick={onClick}>
+              X
+            </div>
+          </div>
+          {children}
+        </div>
+      </div>
+    );
+  };
+
+  return visible ? ReactDOM.createPortal(modal(), document.getElementById('modal') as HTMLElement) : children;
 };
 
 export { ModalWindow };
