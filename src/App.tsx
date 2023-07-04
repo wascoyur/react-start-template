ec026f1e3e39cdeb4faffc235bd67328924fa9d4
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-07-04 14:31:15 +0500

    test buttn

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './components/modal/Modal-window.scss';
 import ModalWindow from './components/modal/ModalWindow';
 import { Header } from './components/header/Header';
 import { Layout } from './components/layout/Layout';
+import { Button } from './components/button/Button';
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -11,7 +12,14 @@ function App() {
     <div className="App">
       <Header></Header>
       <Layout>
-        <button onClick={() => setIsVisible(true)}> Show modal</button>
+        <button onClick={() => setIsVisible(true)}> Show modal</button> <br />
+        <Button label={'В корзину'} backgroundColor="blue" primary={true} />
+        <br />
+        <Button label={'В корзину'} backgroundColor="blue" primary={true} countGood={5} />
+        <div>
+          Start loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong
+          looooooooooooooooooooooooooooooooooong text
+        </div>
       </Layout>
       <ModalWindow
         onClick={() => {
