4f601bd4387c23e7f9aca940f3d4f048936baedc
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-07-10 21:12:10 +0500

    добавлен плагин для исключения ошибок импорта

---
+++ .storybook/main.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  // Other Storybook configuration options...
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.s(a|c)ss$/,
+      include: path.resolve(__dirname, '../'),
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            },
+          },
+        },
+        'sass-loader',
+      ],
+    });
+    return config;
+  },
+};
---
+++ src/stories/Header.stories.jsx
@@ -0,0 +1,8 @@
+import { Header } from 'src/components/header/Header.tsx';
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+export const Default = () => <Header></Header>;
