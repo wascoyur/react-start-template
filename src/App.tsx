64a3f034dba5052e145b5d653b9a99ddb08d360f
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-07-24 20:35:42 +0500

    ДЗ-6. Набросок страницы профиля.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import React, { useState } from 'react';
-import { ProductList } from 'src/homeworks/homework-5/ProductList';
-import { Product } from 'src/homeworks/ts1/3_write';
-import { arrayMockProductsGenerate } from 'src/mock-data/mock-products';
+import React from 'react';
+import { ProfileScreen } from 'src/homeworks/homework-6/ProfileScreen';
 
 function App() {
-  const [products, setProducts] = useState<Array<Product> | undefined>();
-
-  const addMockProduct = (count?: number) => {
-    arrayMockProductsGenerate({ count: 0, currentListProducts: products, setNewProducts: setProducts });
-  };
-
   return (
     <div className="App">
-      <ProductList products={products} />
-      <div style={{ display: 'grid', placeContent: 'center', padding: '2rem' }}>
-        <button onClick={() => addMockProduct()}>Еще...</button>
-      </div>
+      <ProfileScreen />
     </div>
   );
 }
