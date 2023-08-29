31916c9658ee3a37b225436a09889f06dc97243c
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-08-29 16:24:35 +0500

    ДЗ-7-Роутинг. try Observer in Prodict list

--- src/homeworks/homework-5/ProductList.tsx
+++ src/homeworks/homework-5/ProductList.tsx
@@ -8,26 +8,58 @@ export type TypeProductList = { listClssNames?: string; itemClssNames?: string }
 
 export const ProductList: FC<TypeProductList> = (props) => {
   const { listClssNames = 'product-list', itemClssNames = 'product-item' } = props;
-  const [productList, setProductList] = useState<Product[] | undefined>();
+  const [productList, setProductList] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const products = arrayMockProductsGenerate();
-    setProductList(products);
+    const initialProducts = arrayMockProductsGenerate();
+    setProductList(initialProducts);
+    setIsLoading(false);
   }, []);
 
-  const ListItems: FC = () => {
-    return productList ? (
-      productList.map((product) => <ProductItem item={product} key={product.createdAt} />)
-    ) : (
-      <EmptyList />
-    );
+  const loadMoreProducts = () => {
+    setIsLoading(true);
+    const newProduct = arrayMockProductsGenerate();
+    setProductList((productList) => [...productList, ...newProduct]);
+    setIsLoading(false);
   };
 
-  const EmptyList: FC = () => <div>No Products</div>;
+  const handleObserver = (entries: IntersectionObserverEntry[]) => {
+    const entry = entries[0];
+    if (entry.isIntersecting && !isLoading) {
+      loadMoreProducts();
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [isLoading, productList]);
 
   return (
     <div className={classNames(listClssNames)}>
-      <ListItems />
+      <div ref={containerRef}>
+        {productList.map((product, index) => (
+          <ProductItem item={product} key={product.createdAt} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -35,26 +67,13 @@ export const ProductList: FC<TypeProductList> = (props) => {
 export type TypeProductItem = { item: Product; itemClssNames?: string };
 
 export const ProductItem: FC<TypeProductItem> = (props) => {
-  const ref = useRef<HTMLDivElement>(null);
-
   const {
     item: { name, price },
     itemClssNames = 'product-item',
   } = props;
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const [entry] = entries;
-      entry.isIntersecting && arrayMockProductsGenerate();
-    });
-    if (ref.current) observer.observe(ref.current);
-    return () => {
-      if (ref.current) observer.unobserve(ref.current);
-    };
-  }, []);
-
   return (
-    <div className={classNames(itemClssNames)} ref={ref}>
+    <div className={classNames(itemClssNames)}>
       <h4>{name}</h4>
       <p>Price: {price}</p>
     </div>
