482afe7a34ffb996a62f14568bc76a69d608e0b9
yuriy.vasilev <mytem20007@gmail.com>
2023-09-02 11:45:52 +0500

    bugfix после обновления

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,12 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
