3cf7c7fd3f26efae4d9e88a5f7b513af51051133
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-09-02 15:29:01 +0500

    ДЗ-7-Роутинг. bugfix

--- src/stories/components/logo/Logo.tsx
+++ src/stories/components/logo/Logo.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import LogoPicture from '../../assets/logo.jpg';
 import './logo.scss';
 
 export const Logo = () => {
   return (
-    <NavLink to=".">
-      <div className="logo">
-        <img src={LogoPicture} alt="logo" className="logo-image" />
-      </div>
-    </NavLink>
+    <div className="logo">
+      <img src={LogoPicture} alt="logo" className="logo-image" />
+    </div>
   );
 };
