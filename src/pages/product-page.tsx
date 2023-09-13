43796f26cc187e854036902527c46968ef8a621b
yuriy.vasilev <mytem20007@gmail.com>
2023-09-13 23:13:56 +0500

    ДЗ-8-Управление состоянием. Модалку в продуктАйтем

--- src/pages/product-page.tsx
+++ src/pages/product-page.tsx
@@ -1,23 +1,12 @@
 import { ProductList } from 'src/homeworks/homework-5/ProductList';
 import React from 'react';
 import { useStore } from 'src/store/store';
-import { AddProductForm } from 'src/stories/components/product/AddProductForm';
-import ModalWindow from 'src/stories/components/modal/ModalWindow';
 
 export const ProductPage = () => {
   const token = useStore();
   return (
     <>
       <ProductList listClssNames="product-list-item" />
-      {token.tokenAdmin.length && (
-        <ModalWindow
-          modalContent={<AddProductForm />}
-          onClick={() => {
-            console.log('click');
-          }}
-          visible={true}
-        ></ModalWindow>
-      )}
     </>
   );
 };
