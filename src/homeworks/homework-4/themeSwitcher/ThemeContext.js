d694a8e02f07dcca3583027efdfc6d1823a59549
yuriy.vasilev <mytem20007@gmail.com>
2023-09-14 20:49:55 +0500

    ДЗ-8-Управление состоянием. Модалку в продуктАйтем

--- src/homeworks/homework-4/themeSwitcher/ThemeContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-export const ThemeContext = createContext({
-    currentTheme: 'light',
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    togglerTheme: () => { },
-});
-//# sourceMappingURL=ThemeContext.js.map
--- src/pages/home-page.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-export const HomePage = ({ children }) => {
-    return (_jsxs(_Fragment, { children: [_jsx("h1", { children: "\u0414\u043E\u043C\u0430\u0448\u043D\u044F\u044F \u0441\u0442\u0440\u0430\u043D\u0438\u0446\u0430" }), children] }));
-};
-//# sourceMappingURL=home-page.js.map
